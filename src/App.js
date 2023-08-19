0cc803977e8771309f91d41cefaa40948e0b3b9f
Shiven <86053372+Shiven3005@users.noreply.github.com>
2023-08-19 13:59:01 +0530

    Add files via upload
    Project using ReactJs

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+// import "./style.css"
+import Temp from "./components/Temp";
+// import Start from "./start";
+import "./start.css"
+//  import Strt from "./pp";
+//  import IncDec from "./incdecproject";
+  // import "./inc.css"
+  
+
+const App = () =>{
+  
+  return(
+   <>
+  
+   <Temp/>
+   </>
+  );
+  }
+ 
+ 
+
+
+ 
+export default App;
